674aea05d92b043d7fd4141ab0c6e347564c6458
Phil Fitch <mostreluctantly@gmail.com>
2019-08-15 13:57:35 -0700

    html & css static

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import Component from '../Component.js';
+
+class App extends Component {
+    onRender(dom) {
+
+    }
+
+    renderHTML() {
+        return /*html*/`
+            
+        `;
+    }
+}
+
+export default App;
