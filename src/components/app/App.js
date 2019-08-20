f3707351bfc027b3dd5d9d342cd11e87c2ad8aed
Phil Fitch <mostreluctantly@gmail.com>
2019-08-19 22:07:25 -0700

    search and paging set up but buggy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,11 @@
 import Component from '../Component.js';
 import Header from './Header.js';
-import Search from '../options/Search.js';
-import Sort from '../options/Sort.js';
 import PokeDex from '../poke-dex/PokeDex.js';
+import Search from '../options/Search.js';
+import Paging from '../options/Paging.js';
+// import Sort from '../options/Sort.js';
+import { getPokes } from '../../services/pokedex-api.js';
+import hashStorage from '../../services/hash-storage.js';
 
 class App extends Component {
     
@@ -11,19 +14,46 @@ class App extends Component {
         const headerDOM = header.renderDOM();
         dom.prepend(headerDOM);
 
-        const pokeDex = new PokeDex({ pokemon: [] });
-        const pokeDexDOM = pokeDex.renderDOM();
-        const dexSection = dom.querySelector('.pokedex');
-        dexSection.appendChild(pokeDexDOM);
-
-        const url = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                pokeDex.update({ pokemon: data.results });
-            });
-        
         const options = dom.querySelector('.options');
+        const search = new Search();
+        options.appendChild(search.renderDOM());
+
+        const dexSection = dom.querySelector('.pokedex');
+        const paging = new Paging();
+        dexSection.appendChild(paging.renderDOM());
+
+        const pokeDex = new PokeDex({ pokemon: [] });
+        dexSection.appendChild(pokeDex.renderDOM());
+
+        function loadPokes() {
+            const pokes = hashStorage.get();
+            getPokes(pokes)
+                .then(data => {
+                    const pokemon = data.results;
+                    const totalCount = data.count;
+
+                    pokeDex.update({ pokemon: pokemon });
+                    paging.update({
+                        totalCount: totalCount,
+                        currentPage: +pokes.page
+                    });
+                });
+        }
+
+        loadPokes();
+
+        window.addEventListener('hashchange', () => {
+            loadPokes();
+        });
+
+
+        // const url = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
+        // fetch(url)
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         pokeDex.update({ pokemon: data.results });
+        //     });
+        
         // options.appendChild(Search);
         // options.appendChild(Sort);  
 
